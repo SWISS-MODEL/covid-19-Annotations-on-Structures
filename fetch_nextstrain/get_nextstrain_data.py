2f7133ab315dcf802886ade5df024a85e9c3cf12
tomas_masson <tomasmasson0@gmail.com>
2020-04-07 16:54:32 -0300

    Fix json keys. Added output format.

--- fetch_nextstrain/get_nextstrain_data.py
+++ fetch_nextstrain/get_nextstrain_data.py
@@ -12,18 +12,18 @@ def parse_json(input_file):
         '''
         Search recursively across a json dictionary and return the keys corresponding to name, branch_attrs (mutations info), node_attrs (metadata, e.g. authors).
         ''' 
-        for record in input_file['children']:
-            isolate = record['name']                   
-            if 'branch_attrs' in record.keys():
-                mutations = record['branch_attrs']['mutations'] 
-            else:
-                mutations = []
-            if 'node_attrs' in record.keys():
-                metadata = record['node_attrs'] 
-            else:
-                metadata = []
-            array.append([isolate, mutations, metadata])
-            if 'children' in record.keys():
+        root = input_file
+        if 'history' not in root['name'] and 'NODE' not in root['name']:
+            isolate = root['name']
+            mutations_node = root['branch_attrs']['mutations']
+            mutations = [(mutation, mutations_node[mutation]) for mutation in mutations_node if mutation != 'nuc']
+            author = root['node_attrs']['author']
+            gisaig = root['node_attrs']['gisaid_epi_isl']
+            if mutations:
+                for item in mutations:
+                    array.append([isolate, [item], author, gisaig])
+        if 'children' in root:
+            for record in root['children']:
                 parse_node(record, array)
         return array
 
@@ -39,7 +39,29 @@ def get_nextstrain_data():
     results = parse_json(nextstrain_json['tree'])
     return results
 
-if __name__ == '__main__':
+def give_format_output(data):
+    ''' Transform the raw data from nextstrain into a .csv file. ORFs names aer replaced by UniProtKB identifiers.'''
+
+    identifiers = {'ORF1b': 'P0DTD1', 'ORF1a': 'P0DTD1', 'S': 'P0DTC2', 'ORF3a': 'P0DTC3', 'E': 'P0DTC4', 'M': 'P0DTC5', 'ORF6': 'P0DTC6', 'ORF7a': 'P0DTC7', 'ORF7b': 'P0DTD8', 'ORF8': 'P0DTC8', 'N': 'P0DTC9', 'ORF14': 'P0DTD3', 'ORF9b': 'P0DTD2', 'ORF10': 'A0A663DJA2'}
+
+    with open('nextstrain_data.csv', 'w') as fh:
+        headers = f'Protein,Mutation,Isolate,Author,GISAID\n'
+        fh.write(headers)
+        for record in data:
+            if record[1][0][0] in identifiers:
+                protein = identifiers[record[1][0][0]]
+            else:
+                protein = record[1][0][0]
+            mutation = record[1][0][1][0]
+            author = record[2]['value']
+            isolate = record[0]
+            gisaid = record[3]['value']
+            output = f'{protein},{mutation},{isolate},{author},{gisaid}\n'
+            fh.write(output)
+
+def main():
     results = get_nextstrain_data()
-    print(results)
-    
+    give_format_output(results)
+
+if __name__ == '__main__':
+    main()
